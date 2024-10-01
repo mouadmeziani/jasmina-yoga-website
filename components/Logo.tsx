49590777ef853b897500eeba7b1ea1f72efa63e6
mouadmeziani <m.mouadmeziani@gmail.com>
2024-10-01 09:51:36 +0200

    Add Logo

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,13 +1,13 @@
-import { AspectRatio, Flex, Text } from "@chakra-ui/react"
-import ImageLogo from "@/images/logo/websiteLogo.webp"
-import Image from "next/image"
+import { AspectRatio, Flex, Text } from "@chakra-ui/react";
+import ImageLogo from "@/images/logo/JasLogo.png";
+import Image from "next/image";
 
 export function JasminaLogo() {
   return (
-    <AspectRatio ratio={80 / 51} w="80px">
+    <AspectRatio ratio={80 / 51} w="70px">
       <Image src={ImageLogo} alt="logo of the website" width={80} height={51} />
     </AspectRatio>
-  )
+  );
 }
 
 export function Logo() {
@@ -15,10 +15,10 @@ export function Logo() {
     <>
       <Flex align="center">
         <JasminaLogo />
-        <Text fontWeight="bolder" fontSize="lg">
+        <Text fontWeight="bolder" fontSize="lg" ml="4">
           Yoga Jasmina
         </Text>
       </Flex>
     </>
-  )
+  );
 }
